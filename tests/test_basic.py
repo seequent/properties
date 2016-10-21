50dc56410840d9f504d68636c503eca21d804564
Rowan Cockett <rowanc1@gmail.com>
2016-10-20 21:22:06 -0600

    Updates to import statements.
    Arrange things in alphabetical order.
    Keep the standard libs separated.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,11 +3,12 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import properties
-import numpy as np
-import unittest
 import pickle
+import unittest
 import uuid
+
+import numpy as np
+import properties
 
 
 class NumPrimitive(properties.HasProperties):
