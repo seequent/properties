a6995b251831aaf9839424e5a2736de8a4fda9ea
Rowan Cockett <rowan@3ptscience.com>
2016-10-22 19:35:40 -0600

    Bump version: 0.1.5 → 0.2.0

--- properties/__init__.py
+++ properties/__init__.py
@@ -37,7 +37,7 @@ from .basic import (
 from .utils import defaults, undefined
 from .handlers import observe
 
-__version__ = '0.1.5'
+__version__ = '0.2.0'
 __author__ = '3point Science'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2016 3point Science,'
