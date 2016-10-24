4e600b77cf21646d66d1d9b4dafd674e32db36e6
fwkoch <franklin@3ptscience.com>
2016-10-24 14:34:33 -0600

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open("README.rst") as f:
 
 setup(
     name="properties",
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(exclude=('tests',)),
     install_requires=[
         'future',
