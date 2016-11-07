00c042cbeb04ef1bd80d4f2a26487e56e6d8e368
fwkoch <franklin@3ptscience.com>
2016-11-07 11:31:25 -0700

    Update test_list

--- tests/test_list.py
+++ tests/test_list.py
@@ -8,95 +8,112 @@ import unittest
 import properties as props
 
 
-class HasIntA(props.HasProperties):
-    a = props.Integer('int a', required=True)
-
-
-class HasIntList(props.HasProperties):
-    aaa = props.List('list of ints', props.Integer(''))
-
-
-class HasInstanceList(props.HasProperties):
-    aaa = props.List('list of hasinta', props.Instance('', HasIntA))
-
-
-class HasTypeList(props.HasProperties):
-    aaa = props.List('list of hasinta', HasIntA)
-
-
-class HasLengthLists(props.HasProperties):
-    short_list = props.List('short list of hasinta', HasIntA, max_length=3)
-    long_list = props.List('long list of hasinta', HasIntA, min_length=3)
-    constrained_list = props.List('constrained list of hasinta', HasIntA,
-                                  min_length=3, max_length=3)
-
-
-class HasListOfUnion(props.HasProperties):
-    bool_or_color = props.List(
-        'List of bool or color',
-        prop=props.Union('', props=(props.Bool(''), props.Color('')))
-    )
-
-
 class TestList(unittest.TestCase):
 
     def test_list(self):
+
+        with self.assertRaises(TypeError):
+            props.List('bad string list', prop=str)
+        with self.assertRaises(TypeError):
+            props.List('bad max', props.Integer(''), max_length=-10)
+        with self.assertRaises(TypeError):
+            props.List('bad max', props.Integer(''), max_length='ten')
+        with self.assertRaises(TypeError):
+            mylist = props.List('bad max', props.Integer(''), min_length=20)
+            mylist.max_length = 10
+        with self.assertRaises(TypeError):
+            props.List('bad min', props.Integer(''), min_length=-10)
+        with self.assertRaises(TypeError):
+            props.List('bad min', props.Integer(''), min_length='ten')
+        with self.assertRaises(TypeError):
+            mylist = props.List('bad min', props.Integer(''), max_length=10)
+            mylist.min_length = 20
+
+        class HasPropsDummy(props.HasProperties):
+            pass
+
+        mylist = props.List('dummy has props list', prop=HasPropsDummy)
+        assert isinstance(mylist.prop, props.Instance)
+        assert mylist.prop.instance_class is HasPropsDummy
+
+        class HasDummyList(props.HasProperties):
+            mylist = props.List('dummy has props list', prop=HasPropsDummy)
+
+        assert HasDummyList()._props['mylist'].name == 'mylist'
+        assert HasDummyList()._props['mylist'].prop.name == 'mylist'
+
+        class HasIntList(props.HasProperties):
+            aaa = props.List('list of ints', props.Integer(''))
+
         li = HasIntList()
         li.aaa = [1, 2, 3]
         li.aaa = (1, 2, 3)
+        li.aaa = [1., 2., 3.]
+        with self.assertRaises(ValueError):
+            li.aaa = 4
+        with self.assertRaises(ValueError):
+            li.aaa = ['a', 'b', 'c']
 
+        li1 = HasIntList()
+        li2 = HasIntList()
+        assert li1.aaa == li2.aaa
+        assert li1.aaa is not li2.aaa
+
+        class HasConstrianedList(props.HasProperties):
+            aaa = props.List('list of ints', props.Integer(''), min_length=2)
+
+        li = HasConstrianedList()
+        li.aaa = [1, 2, 3]
         li.validate()
-
-        list_instances = (HasInstanceList(), HasTypeList())
-        for li in list_instances[1:]:
-            li.aaa = (HasIntA(), HasIntA())
-            with self.assertRaises(ValueError):
-                li.validate()
-            li.aaa = [{'a':1}, {'a':2}, {'a':3}]
+        li.aaa = [1]
+        with self.assertRaises(ValueError):
             li.validate()
 
-        li = HasLengthLists()
-        li.short_list = [HasIntA(), HasIntA(), HasIntA()]
-        with self.assertRaises(ValueError):
-            li.short_list += [HasIntA()]
-        li.long_list = [HasIntA(), HasIntA(), HasIntA(), HasIntA()]
-        li.long_list = li.long_list[:-1]
-        with self.assertRaises(ValueError):
-            li.long_list  = li.long_list[:-1]
-        li.constrained_list = [HasIntA(), HasIntA(), HasIntA()]
-        with self.assertRaises(ValueError):
-            li.constrained_list  = li.constrained_list[:-1]
-        with self.assertRaises(ValueError):
-            li.constrained_list  += [HasIntA()]
+        class HasConstrianedList(props.HasProperties):
+            aaa = props.List('list of ints', props.Integer(''), max_length=2)
 
-        bcl = HasListOfUnion()
-        bcl.bool_or_color = [True, True, False]
-        bcl.bool_or_color = ['red', [100, 100, 100], '#24415F']
-        bcl.bool_or_color = [True, 'blue']
-        bcl.validate()
-        bcl.bool_or_color[1] = 'green'
-        bcl.validate()
-
+        li = HasConstrianedList()
+        li.aaa = [1, 2]
+        li.validate()
+        li.aaa = [1, 2, 3, 4, 5]
         with self.assertRaises(ValueError):
-            bcl.bool_or_color = 1.5
+            li.validate()
+
+        class HasColorList(props.HasProperties):
+            ccc = props.List('list of colors', props.Color(''))
+
+        li = HasColorList()
+        li.ccc = ['red', '#00FF00']
+        assert li.ccc[0] == (255, 0, 0)
+        assert li.ccc[1] == (0, 255, 0)
+
+        numlist = [1, 2, 3, 4]
+        assert props.List.to_json(numlist) == numlist
+        assert props.List.to_json(numlist) is not numlist
+        assert props.List.from_json(numlist) == numlist
+        assert props.List.from_json(numlist) is not numlist
+
+        class HasIntA(props.HasProperties):
+            a = props.Integer('int a', required=True)
+
+        assert props.List.to_json(
+            [HasIntA(a=5), HasIntA(a=10)]
+        ) == [{'a': 5}, {'a': 10}]
+
+        assert li.serialize() == {'ccc': [[255, 0, 0], [0, 255, 0]]}
+
+        class HasIntAList(props.HasProperties):
+            mylist = props.List('list of HasIntA', HasIntA)
+
+        deser_list = HasIntAList.deserialize(
+            {'mylist': [{'a': 0}, {'a': 10}, {'a': 100}]}
+        ).mylist
+        assert isinstance(deser_list, list)
+        assert len(deser_list) == 3
+        assert isinstance(deser_list[0], HasIntA) and deser_list[0].a == 0
+        assert isinstance(deser_list[1], HasIntA) and deser_list[1].a == 10
+        assert isinstance(deser_list[2], HasIntA) and deser_list[2].a == 100
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
-# def test_list(self):
-    #     array_list = MyListOfArrays()
-    #     array0 = MyArray()
-    #     array1 = MyArray()
-    #     array2 = MyArray()
-    #     assert len(array_list.arrays) == 0
-    #     array_list.arrays = (array0,)
-    #     assert len(array_list.arrays) == 1
-    #     array_list.arrays = [array0, array1, array2]
-    #     assert len(array_list.arrays) == 3
-
-    #     array_list._props['arrays'].assert_valid(array0)
-    #     array0.int_array = [1, 2, 3]
-    #     array_list._props['arrays'].assert_valid(array0)
-    #     array_list._props['arrays'].assert_valid(None)
