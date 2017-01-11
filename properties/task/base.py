93ba3fcc56116e4d3771b8be9d380bf8b2fec1b2
Brendan Smithyman <brendan@3ptscience.com>
2017-01-10 17:28:59 -0700

    Rough out interface for reporting status from a compute task.

--- properties/task/base.py
+++ properties/task/base.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from ..base import HasProperties
-from ..basic import String, Bool
+from ..basic import String, Bool, Float
 
 
 class BaseResult(HasProperties):
@@ -21,11 +21,36 @@ class BaseResult(HasProperties):
     )
 
 
+class TaskStatus(HasProperties):
+    """The present status of the task"""
+
+    progress = Float(
+        'Task progress to completion',
+        required=False
+        min=0,
+        max=1,
+    )
+    message = String(
+        'Task progress message',
+        required=False,
+    )
+
+
 class Task(HasProperties):
     """Computational task"""
 
     _REGISTRY = dict()
     Result = BaseResult
+
+    def report_status(self, status):
+        """Report the task status towards completion"""
+        if not isinstance(status, TaskStatus):
+            raise TypeError('Parameter \'status\' must a TaskStatus')
+        print('{taskname} | {percent:>3}\% | {message}'.format(
+            taskname=self.__class__.__name__,
+            percent=round(100*status.progress),
+            message=status.message if status.message else '',
+        ))
 
     def __call__(self):
         """Execute the compute task"""
