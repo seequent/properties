dd6dfb84a6b2ebf3c1ba42e4df72e9806891242c
Franklin Koch <franklin.koch@seequent.com>
2018-04-11 21:16:39 -0600

    Improve UID tests

--- tests/test_uid.py
+++ tests/test_uid.py
@@ -21,6 +21,8 @@ class TestUID(unittest.TestCase):
         has_uid.uid = 'a'
         assert 'a' in HasUID._INSTANCES
 
+        has_uid.uid = 'a'
+
         with self.assertRaises(properties.ValidationError):
             HasUID(uid='a')
 
@@ -38,33 +40,88 @@ class TestUID(unittest.TestCase):
         assert serial_dict['b'] == {'uid': 'b', 'instance': 'b', '__class__': 'RecursiveUID'}
 
         with self.assertRaises(ValueError):
-            RecursiveUID.deserialize(serial_dict)
+            RecursiveUID.deserialize('hello')
+
+        with self.assertRaises(ValueError):
+            RecursiveUID.deserialize(serial_dict.copy())
 
         RecursiveUID._INSTANCES.clear()
-
-        recursive_uid = RecursiveUID.deserialize(serial_dict)
+        recursive_uid = RecursiveUID.deserialize(serial_dict.copy())
         assert recursive_uid.instance is recursive_uid
-
         assert recursive_uid.validate()
+
+        RecursiveUID._INSTANCES.clear()
+        serial_dict['b']['instance'] = 'c'
+        with self.assertRaises(ValueError):
+            RecursiveUID.deserialize(serial_dict.copy())
 
     def test_pointer(self):
 
+        with self.assertRaises(TypeError):
+            Pointer('', object)
+
+        class AnotherUID(HasUID):
+            pass
+
         class HasPointer(HasUID):
 
-            other = Pointer('', HasUID)
+            other = Pointer('', AnotherUID)
 
         has_pointer = HasPointer()
         has_uid = HasUID()
+        another_uid = AnotherUID()
 
         has_pointer.other = 'silly_pointer'
         assert has_pointer.other == 'silly_pointer'
 
-        has_pointer.other = has_uid
-        assert has_pointer.other is has_uid
+        has_pointer.other = another_uid
+        assert has_pointer.other is another_uid
 
         has_pointer.other = properties.undefined
-        has_pointer.other = has_uid.uid
-        assert has_pointer.other is has_uid
+        has_pointer.other = another_uid.uid
+        assert has_pointer.other is another_uid
+
+        has_pointer.other = properties.undefined
+        with self.assertRaises(properties.ValidationError):
+            has_pointer.other = has_uid
+
+        with self.assertRaises(properties.ValidationError):
+            has_pointer.other = has_uid.uid
+
+    def test_enforce_uid(self):
+
+        with self.assertRaises(TypeError):
+            Pointer('', HasUID, enforce_uid=5)
+
+        class AnotherUID(HasUID):
+            pass
+
+        class HasStrictPointer(HasUID):
+
+            other = Pointer('', AnotherUID, enforce_uid=True)
+
+        has_pointer = HasStrictPointer()
+        has_uid = HasUID()
+        another_uid = AnotherUID()
+
+        with self.assertRaises(properties.ValidationError):
+            has_pointer.other = 'silly_pointer'
+
+        has_pointer.other = another_uid
+        assert has_pointer.other is another_uid
+
+        has_pointer.other = properties.undefined
+        has_pointer.other = another_uid.uid
+        assert has_pointer.other is another_uid
+
+        has_pointer.other = properties.undefined
+        with self.assertRaises(properties.ValidationError):
+            has_pointer.other = has_uid
+
+        with self.assertRaises(properties.ValidationError):
+            has_pointer.other = has_uid.uid
+
+
 
 
 if __name__ == '__main__':
