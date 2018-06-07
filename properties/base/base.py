4c3609f283561c2303c9964123262f43b24ccb12
Franklin Koch <franklin.koch@seequent.com>
2018-06-07 10:22:09 -0600

    Remove unused pickle import

--- properties/base/base.py
+++ properties/base/base.py
@@ -5,7 +5,6 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from collections import OrderedDict
-import pickle
 from warnings import warn
 
 from six import iteritems, itervalues, PY2, string_types, with_metaclass
